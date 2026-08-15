3e76a93281b54e1a74878dcfd702d3d1812545e3
ITHaste <djkevin3107@gmail.com>
2026-08-15 20:39:14 +0200

    stripe fix 2

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,6 +1,7 @@
 // api/create-checkout-session.js
+import Stripe from 'stripe';
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
